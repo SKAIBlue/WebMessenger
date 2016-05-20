0471611fbd1052e3b33b50a7170c8002800a70f3
JongWoo <jwmtp2@gmail.com>
2016-05-20 16:56:04 +0900

    시작 페이지의 스크립트와 미티어 메소드 추가함

---
+++ client/scripts/start.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Anonymous on 2016. 5. 20..
+ * 시작 페이지에서 작동하는 스크립트 입니다
+ */
+Template.StartLayout.events({
+    /**
+     * 시작하기 버튼을 눌렀을 때 작동합니다
+     */
+    "click #start-button": function(){
+        Meteor.call("onClickStartButton");
+    }
+})
