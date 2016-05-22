9c01e44c5091e32fb26e721d4f13581c2cefcfeb
JongWoo <jwmtp2@gmail.com>
2016-05-22 13:13:00 +0900

    페이지 상태 정보를 만들어 템플릿 변경을 용이하게 만들었습니다

--- client/scripts/start.js
+++ client/scripts/start.js
@@ -7,9 +7,10 @@ Template.StartLayout.events({
      * 시작하기 버튼을 눌렀을 때 작동합니다
      */
     "click #start-button": function(){
-        $("#start-button").remove();
-
-        Template.StartLayout.onRendered
+        //$("#start-button").remove();
+        //Template.StartLayout.onRendered
+        // 상태를 회원가입으로 바꿈 ( 템플릿 변경 )
+        Session.set("pageState", PageState.SIGN_UP);
         //Meteor.call("onClickStartButton");
     }
 })
