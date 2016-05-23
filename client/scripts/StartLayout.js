df87294d3e075a6ab98ecf949df4e8beb273aa00
taehoon Kim <kth2573@gmail.com>
2016-05-23 19:10:25 +0900

    StartLayout에서 시작 버튼 회원 가입 폼 추가

---
+++ client/scripts/StartLayout.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Anonymous on 2016. 5. 20..
+ * 시작 페이지에서 작동하는 스크립트 입니다
+ */
+Template.StartLayout.events({
+    /**
+     * 시작하기 버튼을 눌렀을 때 작동합니다
+     */
+    "click #start-button": function(){
+        $("#start-button").remove();
+        var LoginLayout = Template.LoginLayout;
+        var LoginId = document.getElementById('LoginForm');
+        Blaze.render(LoginLayout, LoginId); // 탬플릿 랜더링
+        $('.at-title').remove();            //at-form 타이틀 제거
+        $('#at-btn').addClass('light-blue');//버튼색상 변경
+        // 상태를 회원가입으로 바꿈 ( 템플릿 변경 )
+        Session.set("pageState", PageState.CHAT);
+    }
+})
