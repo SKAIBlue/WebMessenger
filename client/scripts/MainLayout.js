4e71531591cf00616ef867ddb73912738f00ba9c
KTHDNA <kth2573@gmail.com>
2016-05-27 14:29:30 +0900

    MainLayout.html 약간 수정함. 큰 변경 사항 없음

--- client/scripts/MainLayout.js
+++ client/scripts/MainLayout.js
@@ -2,4 +2,11 @@
  * Created by kth on 16. 5. 23.
  */
 // TODO  1. Main 페이지에서 만약,닉네임이 없다면 닉네임을 정하도록 하는 화면 띄우기
+Template.MainLayout.events({
+    "":function(){
+        alert("Test view ");
+    }
+});
+Template.MainLayout.helpers({
 
+});
