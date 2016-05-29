201a4ea2e055884fc916585be40eb1f85779da3d
JongWoo <jwmtp2@gmail.com>
2016-05-29 17:49:06 +0900

    파일 업로드 템플릿 예 제작 완료

--- client/scripts/FileUpload.js
+++ client/scripts/FileUpload.js
@@ -24,8 +24,15 @@ var chatRoom =  {
     },
     prefix:this._id,
     id:this._id
-
 }
+
+Template.ChooseImage.created = function() {
+    Uploader.init(this);
+}
+
+Template.ChooseImage.rendered = function () {
+    Uploader.render.call(this);
+};
 
 Template.ChooseImage.helpers({
     profile()
@@ -36,14 +43,15 @@ Template.ChooseImage.helpers({
     {
         return chatRoom;
     },
-    fileTypes:'image/*'
+    imageFileType:'image/*',
+    allFileType:''
+
 });
 
 Template.ChooseImage.events({
     'click .start'(e)
     {
-        console.log(Template.instance(), e);
-        Uploader.startUpload(Template.instance(), e);
+        Uploader.startUpload.call(Template.instance(), e);
     }
 });
 
@@ -73,3 +81,38 @@ Template.InvisibleFileDorpZone.helpers({
 Template.InvisibleFileDorpZone.events({
 
 });
+
+
+Template.customUpload.created = function() {
+    Uploader.init(this);
+}
+
+Template.customUpload.rendered = function () {
+    Uploader.render.call(this);
+};
+
+Template.customUpload.events({
+    'click .start': function (e) {
+        Uploader.startUpload.call(Template.instance(), e);
+    }
+});
+
+Template.customUpload.helpers({
+    'infoLabel': function() {
+        var instance = Template.instance();
+        // we may have not yet selected a file
+        var info = instance.info.get()
+        if (!info) {
+            return;
+        }
+        var progress = instance.globalInfo.get();
+
+        // we display different result when running or not
+        return progress.running ?
+        info.name + ' - ' + progress.progress + '% - [' + progress.bitrate + ']' :
+        info.name + ' - ' + info.size + 'B';
+    },
+    'progress': function() {
+        return Template.instance().globalInfo.get().progress + '%';
+    }
+})
