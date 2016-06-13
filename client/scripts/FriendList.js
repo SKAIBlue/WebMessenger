63d6ff8b54c9cc882af5780b7969fbb6da8b48aa
KTHDNA <kth2573@gmail.com>
2016-06-13 19:33:05 +0900

    친구 초대 버튼 구현

--- client/scripts/FriendList.js
+++ client/scripts/FriendList.js
@@ -10,4 +10,14 @@ Template.FriendList.events({
 
 Template.FriendList.helpers({
 
-})
+});
+
+Template.ModalInviteFriendList.helpers({
+    myfriend_list()
+    {
+        return [{
+            friend_name : "김태훈",            //나의 친구 목록 중 이름
+            friend_img : "img/default_profile.png" // 프로필
+        }];
+    }
+});
