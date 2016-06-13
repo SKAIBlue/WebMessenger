9050b0fd5bd8ad9db7d3fae89769e07862157f79
JongWoo <jwmtp2@gmail.com>
2016-06-13 17:00:44 +0900

    Merge branches 'FriendScript' and 'friend_list' of https://github.com/SKAIBlue/WebMessenger into FriendScript
                    # Conflicts:
                    #	client/htmls/MainLayout.html
                    #	client/scripts/MainLayout.js
                    채팅 내용 표시

--- client/scripts/ChatLayout.js
+++ client/scripts/ChatLayout.js
@@ -2,27 +2,49 @@
  * Created by Anonymous on 2016. 6. 13..
  */
 
+function updateDom()
+{
+    Session.set(SESSION_UPDATE, Session.get(SESSION_UPDATE) + 1);
+}
+
+
+function dateFormat(date)
+{
+    var now = new Date();
+    if(now.getFullYear() == date.getFullYear() &&
+    now.getMonth() == date.getMonth() &&
+    now.getDay() == date.getDay())
+    {
+        return ""
+    }
+    return now;
+}
+
 function getChatList(selectedChatRoom)
 {
-    var chat = Chat.find({roomId:selectedChatRoom});
-    return chat;
+    var chat = Chat.find({chatRoomId:selectedChatRoom});
     var result = [];
-    console.log("getChatList");
-    chat.forEach(c)
-    {
+    chat.forEach(function(item){
+        var user = UserAddition.findOne({userId:item.chatUserId});
+        var profile = user.profile.length == 0 ? PROFILE_DEFAULT_PATH : user.profile;
+        var nickName = user.nickName.length == 0 ? user.email : user.nickName;
+        var time = dateFormat(item.chatTime);
         result.push({
-            chatWho:c.chatWho,
-            chatProfile:c.chatProfile,
-            chatText:c.chatText,
-            chatTime:c.chatTime,
+            chatWho:nickName,
+            chatProfile:profile,
+            chatText:item.chatText,
+            chatTime:time,
             isMy(){
-                return Meteor.userId == c.chatWho;
+                return Meteor.userId() == item.chatUserId;
             },
             position(){
                 return (this.isMy()) ? "me":"you";
             }
         });
-    }
+    });
+    console.log("before update");
+    updateDom();
+    console.log("after update");
     return result;
 }
 
@@ -38,7 +60,7 @@ function sendMessage(message)
         console.log("not select chat room");
         return;
     }
-    // 채팅 내용 저장
+
     Chat.insert({
         chatUserId: Session.get(SESSION_USER_ID),
         chatRoomId: selectedChatRoom,
@@ -52,8 +74,14 @@ function sendMessage(message)
 Template.ChatLayout.helpers({
     chat_list(){
         return getChatList(Session.get(SESSION_SELECTED_CHAT_ROOM));
+    },
+    update()
+    {
+        console.log("update helper");
+        return Session.get(SESSION_UPDATE);
     }
 });
+
 
 Template.ChatLayout.events({
     'submit .input-form'(event)
