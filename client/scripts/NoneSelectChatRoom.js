7f05f5e05cb08f7cb05579a8973b51dc1533837e
KTHDNA <kth2573@gmail.com>
2016-06-15 15:06:12 +0900

    글꼴 추가

--- client/scripts/NoneSelectChatRoom.js
+++ client/scripts/NoneSelectChatRoom.js
@@ -7,21 +7,23 @@ Template.NoneSelectChatRoom.helpers({
        console.dir(MyAddition);
        if(MyAddition.chatRoomList.length == 0 && MyAddition.friends.length == 0)
        {
-           $('#helpText').append("반갑습니다!</br><i class='medium material-icons'>face</i> 를 눌러 친구를 추가한후" +
-               "</br><i class='medium material-icons'>add</i>를 눌러 추가된 친구와 대화를 시작하세요!");
+           $('#helpText').append("<div id='helpTextArea'>반갑습니다!</br><i class='medium material-icons'>face</i> 를 눌러 친구를 추가한후" +
+               "</br><i class='medium material-icons'>add</i>를 눌러 추가된 친구와 대화를 시작하세요!</div>");
            return;
        }
        else if(MyAddition.friends.length == 0 )
        {
-           $('#helpText').append("<i class='medium material-icons'>face</i> 를 눌러서 친구를 추가하세요");
+           $('#helpTextArea').remove();
+           $('#helpText').append("<div id='helpTextArea'><i class='medium material-icons'>face</i> 를 눌러서 친구를 추가하세요</div>");
            return;
        }
        else if(MyAddition.chatRoomList.length == 0)
        {
-           $('#helpText').append("<i class='medium material-icons'>add</i> 를 눌러서 대화를 시작하세요");
+           $('#helpTextArea').remove();;
+           $('#helpText').append("<div id='helpTextArea'><i class='medium material-icons'>add</i> 를 눌러서 대화를 시작하세요</div>");
            return;
        }
-       $('#helpText').append("<i class='medium material-icons'>face</i> 를 눌러서 대화를 시작하세요");
+       $('#helpText').append("반갑습니다!");
        return;
    }
 });
